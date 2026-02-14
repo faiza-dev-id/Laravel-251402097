54caf586ac32df050c533aaef4f0520acb8205cd
faiza-dev-id <m.faiza.elfaz@gmail.com>
2026-02-14 15:34:19 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
                     <div style="text-align:center;margin-top:120px">
-                            <h6>Muhammad Faiza Elfaz</h6>
+                            <h6></h6>
                             <h5>251402097</h5>
                         </div>
                         
